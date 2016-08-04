d6bbc7397c6fa77e1b8c506fee699b686d0b01ef
Colby Taperts <colbytaperts@gmail.com>
2016-08-04 08:32:01 -0500

    update views

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'paperclip'
+
+gem "paperclip", "~> 5.0.0"
 
 gem 'bootstrap-sass'
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+  has_attached_file :avatar,
+  styles: {
+  medium: "300x300#",
+  thumb: "50x50" },
+  default_url: "missing.jpg"
 
-  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png" 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :avatar, less_than: 3.megabytes, unless: Proc.new { |m| m[:avatar_file_name].blank?}
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
