a95e73d081ed7e687a3253cebdd7935016a50d2a
Colby Taperts <colbytaperts@gmail.com>
2016-08-08 10:37:37 -0500

    add shop dropdown with categories

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,8 +1,17 @@
 class StorefrontController < ApplicationController
   def index
+
+    if params[:cat_id]
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else
     @products = Product.all
+    end
   end
 
   def about
+
   end
+
 end
+
