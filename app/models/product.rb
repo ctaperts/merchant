b9384744d330bc9c3398d79574c6d92c1f2b7710
Colby Taperts <colbytaperts@gmail.com>
2016-08-08 12:05:40 -0500

    update product view, fix submit button in form

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   styles: {
   medium: "300x300#",
   thumb: "50x50" },
-  default_url: "missing.jpg"
+  default_url: "missing_:style.jpg"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates_attachment_size :avatar, less_than: 3.megabytes, unless: Proc.new { |m| m[:avatar_file_name].blank?}
