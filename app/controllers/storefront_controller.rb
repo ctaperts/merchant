1983f7c5fd445a95a0afc7ee5749fa458607ca59
Colby Taperts <colbytaperts@gmail.com>
2016-08-08 11:02:23 -0500

    add search to navbar

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,12 +1,21 @@
 class StorefrontController < ApplicationController
   def index
 
-    if params[:cat_id]
+    if params[:search]
+      @products = Product.search_by_name_or_description(params[:search])
+      if @products.empty?
+	flash.now[:notice] = "No Result found for \"#{params[:search]}\", showing all products"
+	@products = Product.all
+      end
+
+    elsif params[:cat_id]
       @category = Category.find(params[:cat_id])
       @products = Product.where(category_id: params[:cat_id])
     else
     @products = Product.all
     end
+
+
   end
 
   def about
