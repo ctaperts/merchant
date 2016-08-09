1a21167b4ae7a596c1d62027c62c52736841b6e3
Colby Taperts <colbytaperts@gmail.com>
2016-08-09 09:19:59 -0500

    setup models for cart and line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_attached_file :avatar,
   styles: {
   medium: "300x300#",
@@ -17,4 +20,17 @@ class Product < ApplicationRecord
   def self.search_by_name_or_description(string)
     where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%" )
   end
+
+  private 
+
+  #ensure no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'lines items present')
+      return false
+    end
+  end
+
 end
