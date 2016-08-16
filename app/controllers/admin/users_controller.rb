d02f934e41b8550d401d9528e080a556de461ae7
Colby Taperts <colbytaperts@gmail.com>
2016-08-15 20:12:34 -0500

    fix set before

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,5 @@
 class Admin::UsersController < ApplicationController
-  #before_action :set_user, only: [:show, :edit, :update, :destroy, :create, :new]
-  # TODO what is before action?
+  before_action :set_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
   def index
@@ -8,11 +7,12 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
+    #@users = User.find(params[:id])
 
-    if @user.destroy && !current_user
-	@user.destroy
-        redirect_to admin_users_path, notice: "User deleted."
+    if @users.destroy
+        redirect_to :back, notice: "User deleted."
+    else
+        redirect_to :back, notice: "Error: User not deleted."
     end
   end
 
@@ -21,36 +21,36 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-    @users = User.find(params[:id])
+    #@users = User.find(params[:id])
   end
 
   def update
-  @users = User.find(params[:id])
+  #@users = User.find(params[:id])
     if @users.update(user_params)
       redirect_to :back, notice: "Successfully updated"
     else
-      flash.now[:notice] = "Something went wrong could not update"
+      redirect_to :back, notice: "Error: User not updated"
     end	
   end
 
   def create
     @users = User.new(user_params)
     if @users.save
-      redirect :back, notice: "Successfully saved!"
+      redirect_to :back, notice: "Successfully saved!"
     else
       flash.now[:notice] = "Something went wrong could not save the user"
-      redirect_to :back
+      redirect_to :back, notice: "Something went wrong could not save the user"
     end
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  #def set_user
-    #@user = User.find(params[:id])
-  #end
+  def set_user
+    @users = User.find(params[:id])
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
