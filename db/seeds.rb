be87d6b771d180f0ed8b1f0635843e39693e5695
Colby Taperts <colbytaperts@gmail.com>
2016-08-16 23:34:34 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+brands = ['generic']
+
+categories = ['Produce',
+	      'Fruits',
+	      'Greens',
+	      'Juices',
+	      'Dairy & Cheese',
+	      'Seasons & Dressings']
+
+products = [ ['Strawberry', 1, 0, 'Strawberry Description', 2, 1 ] ]
+
+brands.each do |brand|
+  Brand.create(name: brand)
+end
+
+categories.each do |category|
+  Category.create(name: category)
+end
+
+products.each do |product|
+  Product.create(name: product[0])
+  Product.create(price: product[1])
+  Product.create(quantity: product[2])
+  Product.create(description: product[3])
+  Product.create(category_id: product[4])
+  Product.create(brand_id: product[5])
+end
+
