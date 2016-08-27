d97c65d507b70d98b74f3a737ac0643139cbb1f4
Colby Taperts <colbytaperts@gmail.com>
2016-08-27 00:04:12 -0500

    possibly fix mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,38 +20,18 @@ class OrdersController < ApplicationController
     @order = Order.where(user_id: current_user.id)
   end
 
-  #def create
-    #@order = Order.new(order_params)
-    #@order.user_id = current_user.id
-    #@order.add_line_items_from_cart(@cart)
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.add_line_items_from_cart(@cart)
 
-    #if @order.save
-      #Cart.destroy(session[:cart_id])
-      #session[:cart_id] = nil
+    if @order.save
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
       #deliver the order confirmation e-mail
       #OrderConfirmationMailer.send_order_confirmation(current_user, @order).deliver_now
-      #redirect_to shop_url, notice: "Thanks for your order"
-    #else
-      #render :new
-    #end
-  #end
-
-  def create
-    @order = current_user.orders.build(order_params)
-  
-    if @order.save
-      # Move items from cart to order
-      @order.add_line_items_from_cart(@cart.id)
-  
-      # Destroy the cart and reset the session cart_id
-      @cart.destroy
-      session[:cart_id] = nil
-  
-      # Deliver order confirmation email
       OrderConfirmation.send_order_confirmation(current_user, @order).deliver_now
-
-      # Redirect and confirmation notice
-      redirect_to @order, notice: "Thank you for your order!"
+      redirect_to shop_url, notice: "Thanks for your order"
     else
       render :new
     end
