8beebc5d6b2f109efc8bd18e665b689b4e562ae3
Colby Taperts <colbytaperts@gmail.com>
2016-09-04 18:00:04 -0500

    add comments to product helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,6 @@
 module ProductsHelper
 
+  # per item is 1, 3, 5, 7
   def per_item
     @product = Product.find(params[:id])
     @product.item_amount_type += 1
@@ -13,6 +14,7 @@ module ProductsHelper
     render :edit
   end
 
+  # per pound is 2, 3, 6, 7
   def per_pound
     @product = Product.find(params[:id])
     @product.item_amount_type += 2
@@ -26,6 +28,7 @@ module ProductsHelper
     render :edit
   end
 
+  #per case is 4, 5, 6, 7
   def per_case
     @product = Product.find(params[:id])
     @product.item_amount_type += 4
