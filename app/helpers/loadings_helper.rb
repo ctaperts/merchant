7ef23f80f2fd3902e02e1a6abf533678db3665c1
Colby Taperts <colbytaperts@gmail.com>
2016-09-08 23:30:09 -0500

    add helper for quality check

--- app/helpers/loadings_helper.rb
+++ app/helpers/loadings_helper.rb
@@ -1,9 +1,15 @@
 module LoadingsHelper
   def quality_check
     @item = LineItem.find(params[:id])
-    @item.update_columns({quality_check: true, quality_check_by: current_user.email, quality_check_on: Time.now})
-    @item.save
-    redirect_to(:back)
+    if @item.quality_check == true
+      @item.update_columns({quality_check: false, quality_check_by: nil, quality_check_on: nil})
+      @item.save
+      redirect_to(:back)
+    else
+      @item.update_columns({quality_check: true, quality_check_by: current_user.email, quality_check_on: Time.now})
+      @item.save
+      redirect_to(:back)
+    end
   end
   def load_check
     @item = LineItem.find(params[:id])
