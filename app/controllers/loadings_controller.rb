fdcb6e5f5d761a6452679b077e5e1ed549317953
Colby Taperts <colbytaperts@gmail.com>
2016-09-11 12:43:59 -0500

    fix the sort by location, cleanup loadings controller

--- app/controllers/loadings_controller.rb
+++ app/controllers/loadings_controller.rb
@@ -1,18 +1,16 @@
 class LoadingsController < ApplicationController
   include LoadingsHelper
   def index
-    @order = Order.all.reverse_order
     @route_order_ids = Route.all.where(truck_loaded: false).pluck(:order_id)
   end
   def show_all
     @order = Order.all.reverse_order
   end
   def show_by_order
-    @order = Order.all.reverse_order
     @route_order_ids = Route.all.where(truck_loaded: false).pluck(:order_id)
   end
   def show_by_location
-    @order = Order.all.reverse_order
     @route_order_ids = Route.all.where(truck_loaded: false).pluck(:order_id)
+    @line_item_by_location = LineItem.where(order_id: @route_order_ids).sort_by{|item| item.product.location.name}
   end
 end
