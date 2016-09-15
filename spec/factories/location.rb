b97921bb7d98ad734859b122c3c5677d2c7058b0
Colby Taperts <colbytaperts@gmail.com>
2016-09-14 23:22:51 -0500

    setup location tests

--- spec/factories/location.rb
+++ spec/factories/location.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory(:location) do |f|
     f.name { Faker::Name.name }
-    f.product_id { Faker::Number(10) }
+    f.product_id { Faker::Number.number(10) }
   end
 end
